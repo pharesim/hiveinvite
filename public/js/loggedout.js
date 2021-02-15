537a6be5b27032f47782f99dd35d31cb05ccf630
pharesim <pharesim@protonmail.com>
2021-02-15 18:33:07 +0000

    hivejs

--- public/js/loggedout.js
+++ public/js/loggedout.js
@@ -6,14 +6,14 @@ document.getElementById('loginButton').onclick = function() {
 document.getElementById('loginNow').onclick = function() {
   let loginUser = getValueById('loginUsername');
   let loginKey  = getValueById('loginPostingKey');
-  if(steem.auth.isWif(loginKey)) {
+  if(hive.auth.isWif(loginKey)) {
     loginKey = loginKey;
   } else {
-    loginKey = steem.auth.toWif(loginUser, loginKey, 'posting');
+    loginKey = hive.auth.toWif(loginUser, loginKey, 'posting');
   }
 
-  let pub = steem.auth.wifToPublic(loginKey);
-  steem.api.getAccounts([loginUser], function(err, result) {
+  let pub = hive.auth.wifToPublic(loginKey);
+  hive.api.getAccounts([loginUser], function(err, result) {
     let keys = result[0]['posting']['key_auths'];
     for (var i = 0; i < keys.length; i++) {
       if(keys[i][0] == pub) {
