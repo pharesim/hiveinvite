4fe878d0606eeeb79d29d0775b1f778789532026
pharesim <pharesim@protonmail.com>
2021-02-15 18:38:20 +0000

    hivejs

--- public/js/loggedout.js
+++ public/js/loggedout.js
@@ -6,14 +6,14 @@ document.getElementById('loginButton').onclick = function() {
 document.getElementById('loginNow').onclick = function() {
   let loginUser = getValueById('loginUsername');
   let loginKey  = getValueById('loginPostingKey');
-  if(hive.auth.isWif(loginKey)) {
+  if(hivejs.auth.isWif(loginKey)) {
     loginKey = loginKey;
   } else {
     loginKey = hive.auth.toWif(loginUser, loginKey, 'posting');
   }
 
   let pub = hive.auth.wifToPublic(loginKey);
-  hive.api.getAccounts([loginUser], function(err, result) {
+  hivejs.api.getAccounts([loginUser], function(err, result) {
     let keys = result[0]['posting']['key_auths'];
     for (var i = 0; i < keys.length; i++) {
       if(keys[i][0] == pub) {
