3866d37a10ebd8dd6510efa675d934f34149c905
pharesim <pharesim@protonmail.com>
2021-02-15 18:37:49 +0000

    hivejs

--- public/js/freeclaim.js
+++ public/js/freeclaim.js
@@ -1,6 +1,6 @@
 function validateFreeClaimModalForm() {
   let value = getValueById('freeClaimActiveKey');
-  if(hive.auth.isWif(value)) {
+  if(hivejs.auth.isWif(value)) {
     document.getElementById('singleFreeClaim').disabled = false;
     document.getElementById('botFreeClaim').disabled = false;
     return true;
